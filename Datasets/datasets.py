49e4b7fdb7d8815c90eb1e8dcce161399d2fb291
darshitttt <theshit.pandya@gmail.com>
2024-04-02 17:12:39 +0200

    Added the code for TUT18 Dataset too

--- Datasets/datasets.py
+++ Datasets/datasets.py
@@ -5,7 +5,7 @@ import torchaudio
 import pandas as pd
 import numpy as np
 
-
+# Synthetic Scrapper Dataset with events and scenes
 class scraperDataset(Dataset):
 
     '''datadir = '../../audioData/sythenticSoundscenes/test/'
@@ -42,4 +42,30 @@ class scraperDataset(Dataset):
         return sample
 
 
+# Real-world TUTUrban18 Dataset with Scene Labels
+class TUT18_Dataset(Dataset):
 
+    '''datadir = '../../audioData/TUTUrban2018/developmentDataset/TUT-urban-acoustic-scenes-2018-development/'
+    csv = 'TUT18_train.csv'''
+
+    def __init__(self, dataset_csv, data_dir):
+
+        self.dataset_csv = dataset_csv
+        self.data_directory = data_dir
+        self.dataframe = pd.read_csv(dataset_csv)
+
+        self.audio_files = self.dataframe['files']
+        self.scene_labels = self.dataframe['labels']
+
+    def __len__(self):
+        return len(self.audio_files)
+    
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        audio_file = os.path.join(self.data_directory, self.audio_files[idx])
+        audio_data, sr = torchaudio.load(audio_file)
+
+        sample = {'audio':audio_data, 'scene_label':self.scene_labels[idx]}
+        return sample
